2d151530e9f99ff816cab0b6be93baf8193f435f
Alex Sychov <alex.sychov@hexagon.com>
2022-05-17 15:54:47 +0300

    fixed headers

--- api.js
+++ api.js
@@ -3,32 +3,40 @@ export default class Api {
     return fetch(path, {
       method: "GET",
 
-      "Content-Type": "application/json",
-      "X-Master-Key": process.env.REACT_APP_TOKEN,
+      headers: {
+        "Content-Type": "application/json",
+        "X-Master-Key": process.env.REACT_APP_TOKEN,
+      },
     });
   }
 
   static fetchPostRequest(path, body) {
     return fetch(path, {
       method: "POST",
-      "Content-Type": "application/json",
-      "X-Master-Key": process.env.REACT_APP_TOKEN,
+      headers: {
+        "Content-Type": "application/json",
+        "X-Master-Key": process.env.REACT_APP_TOKEN,
+      },
       body,
     });
   }
   static fetchPutRequest(path, body) {
     return fetch(path, {
       method: "PUT",
-      "Content-Type": "application/json",
-      "X-Master-Key": process.env.REACT_APP_TOKEN,
+      headers: {
+        "Content-Type": "application/json",
+        "X-Master-Key": process.env.REACT_APP_TOKEN,
+      },
       body,
     });
   }
   static fetchDeleteRequest(path, body) {
     return fetch(path, {
       method: "DELETE",
-      "Content-Type": "application/json",
-      "X-Master-Key": process.env.REACT_APP_TOKEN,
+      headers: {
+        "Content-Type": "application/json",
+        "X-Master-Key": process.env.REACT_APP_TOKEN,
+      },
       body,
     });
   }
